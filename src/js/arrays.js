d755a25e35a3043ee5d32d9f7924775a0e04f79f
santakratos <santakratos10@gmail.com>
2022-08-04 23:42:57 +0300

    v1.2

--- src/js/arrays.js
+++ src/js/arrays.js
@@ -70,7 +70,7 @@ function some(array, callback) {
   for (let i = 0; i < array.length; i++) {
     const callbackResult = callback(array[i], i, array);
     if (callbackResult) {
-      return callbackResult;
+      return true;
     }
   }
   return false;
@@ -86,7 +86,7 @@ function every(array, callback) {
   for (let i = 0; i < array.length; i++) {
     const callbackResult = callback(array[i], i, array);
     if (!callbackResult) {
-      return callbackResult;
+      return false;
     }
   }
   return true;
